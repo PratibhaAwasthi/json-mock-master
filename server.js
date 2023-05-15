8bc0a6ccae601fe238979d3afd37cd71cdbee816
PratibhaAwasthi <pratibhaawasthi17@gmail.com>
2023-05-14 22:40:21 -0700

    addition json

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router1 = jsonServer.router("db.json");
+const router2 = jsonServer.router("db.json2");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use("", router1);
+server.use("", router2);
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
